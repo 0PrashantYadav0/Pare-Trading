34542762a6641c2773498f4edee1e43feebe4820
Prashant <prashantyadav09783@gmail.com>
2024-02-10 19:38:42 +0530

    poster

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import recording from "../assets/recording.mov"
+import tech1 from "../assets/tech1.avif"
 
 export function VideoPlayer() {
   return (
     <>
-      <video loop muted autoPlay className="h-[600px] border-y-2 border-black">
+      <video loop muted autoPlay poster={tech1} className="h-[600px] border-y-2 border-black">
         <source src={recording}/>
       </video>
     </>
