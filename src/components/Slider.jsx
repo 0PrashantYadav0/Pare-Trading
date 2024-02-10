3a8bdbe29951d472631c28d20c531ce5a4b09c6d
Prashant <prashantyadav09783@gmail.com>
2024-02-10 19:45:33 +0530

    "responsive"

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -5,7 +5,7 @@ import tech1 from "../assets/tech1.avif"
 export function VideoPlayer() {
   return (
     <>
-      <video loop muted autoPlay poster={tech1} className="h-[600px] border-y-2 border-black">
+      <video loop muted autoPlay poster={tech1} className="lg:h-[800px] xl:h-[1100px] border-y-2 border-black h-[400px] md:h-[600px]">
         <source src={recording}/>
       </video>
     </>
