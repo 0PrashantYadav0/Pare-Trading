527ee5faef5e88dd366fff4512d5faba8dfd6347
Prashant <prashantyadav09783@gmail.com>
2024-02-11 10:30:56 +0530

    video file change

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -5,8 +5,7 @@ import tech1 from "../assets/tech1.avif"
 export function VideoPlayer() {
   return (
     <>
-      <video loop muted autoPlay poster={tech1} className="lg:h-[700px] border-y-2 border-black sm:h-[500px] h-[400px] md:h-[600px]">
-        <source src={recording}/>
+      <video src={recording} type="video/mov" loop muted autoPlay poster={tech1} className="lg:h-[600px] border-y-2 border-black sm:h-[500px] h-[400px] md:h-[600px]">
       </video>
     </>
   );
